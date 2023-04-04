cb03df7308b57e255253b577892c20cd04a93799
aramoh3ni <alireza.mohseni.se@gmail.com>
2023-04-04 22:47:40 +0430

    adding sidear category filtering options.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ import Sidebar from "./Sidebar"
 
 const styles = {
   main: `bg-primary`,
-  sidebarWrapper: `fixed top-0 left-0 z-40 w-80 min-h-screen pt-20 border-r-default border-gray-600`,
+  sidebarWrapper: `fixed top-0 left-0 z-40 w-80 min-h-screen pt-16 border-r-default border-gray-600`,
   contentWrapper: `pt-20 px-4 pb-4 sm:ml-80 min-h-screen`,
 }
 
